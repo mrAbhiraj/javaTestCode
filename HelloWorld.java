9aa0cb751f855614ee76aeca5edaad330d676351
mrAbhiraj <129583565+mrAbhiraj@users.noreply.github.com>
2026-04-26 17:02:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Abhishek and Rinki learning devops");
+        System.out.println("Hello, Abhishek and Rinki learning devops 🙂");
     }
 }
